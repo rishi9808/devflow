7598495e9b3b6ecb226bbbc9ca8731e4406502ab
Rishikesh K V <rishikeshkaroth@gmail.com>
2024-12-05 13:48:23 +0530

    Tailwind Css Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] text-red-300 sm:p-20">
-      Helloo
+    <div>
+      <h1 className="h1-bold">Hello form Nextjs</h1>
+      <h2 className="h2-semibold">hi dasdassc</h2>
     </div>
   );
 }
