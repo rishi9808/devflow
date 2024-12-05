741becb46371016a821091a8a7bf694b38ac18f9
Rishikesh K V <rishikeshkaroth@gmail.com>
2024-12-05 12:28:28 +0530

    ESLint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     Helloo
+    <div className="min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] text-red-300 sm:p-20">
+      Helloo
     </div>
   );
 }
